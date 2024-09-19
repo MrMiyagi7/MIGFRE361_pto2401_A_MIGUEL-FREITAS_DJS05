4b50159930d0866dbc578a7778240c59f5903588
MrMiyagi7 <157651526+MrMiyagi7@users.noreply.github.com>
2024-09-19 13:27:47 +0200

    Created reducer function

--- store.js
+++ store.js
@@ -1,2 +1,15 @@
 // Intialises initial count
 const initialcount = 0;
+
+const tallyReducer = (state = initialcount, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    case "RESET":
+      return 0;
+    default:
+      return state;
+  }
+};
